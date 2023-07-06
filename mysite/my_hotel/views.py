0f950077612c2f043e2cfbe2c98af85ca04b47b2
KamylovMurad <126114133+KamylovMurad@users.noreply.github.com>
2023-07-07 03:12:06 +0400

    Update views.py

--- mysite/my_hotel/views.py
+++ mysite/my_hotel/views.py
@@ -1,5 +1,4 @@
 from typing import List
-
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
